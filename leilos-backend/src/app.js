3f4d58b8550843266f17a7fa57f089435920bad7
Mathias Voelcker <mathias_v94@Mathiass-MacBook-Pro.local>
2018-07-03 22:17:47 -0300

    remover writes inuteis que só quebravam a aplicação

--- leilos-backend/src/app.js
+++ leilos-backend/src/app.js
@@ -8,11 +8,14 @@ var mockLances = require('./mocks/mockLances');
 
 //SOCKET
 var net = require('net');
+JsonSocket = require('json-socket');
 
-var HOST = '192.168.0.3';
-var PORT = 3000;
+
+var HOST = '192.168.0.9';
+var PORT = 1234;
 
 var client = new net.Socket();
+var socket = new JsonSocket(client);
 //
 const app = express();
 
@@ -57,6 +60,7 @@ app.post('/addLance', (req, res) => {
 	var novoLance = new Lancamento(body);
 	listLances.push(novoLance);
   res.json({status:'ok'});
+  socket.sendMessage({a: 5, b: 7});
   client.emit('novoLance', { lance: novoLance });
     // client.on('novoLance', function() {
     //     console.log('Client connected to: ' + HOST + ':' + PORT);
@@ -73,13 +77,19 @@ app.listen(3000, () => console.log('Leilões rodando na porta 3000!'));
 client.connect(PORT, HOST, function() {
     console.log('Client connected to: ' + HOST + ':' + PORT);
     // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
-    client.write('Hello World!');
-    client.write('Hello DO MATHIAS!');
+    // client.write('Hello World!');
+    // client.write('Hello DO MATHIAS!');
+    socket.sendMessage({
+      nomeProduto: "Gameboy",
+      nomeComprador: "Seu zé",
+      valor: 250
+      });
+    // client.emit('novoLance', {a: 5, b: 7});
 });
 
-client.on('novoLance', function(data) {
-    console.log(data);
-    app.get('/ultimoLance', (req, res) => {
-      res.json(data)
-    });
-});
+// client.on('novoLance', function(data) {
+//     client.write("Comprador " + data['lance'].nomeComprador + " fez lançamento de R$ " + data['lance'].valor + " para o produto " + data['lance'].nomeProduto);
+//     app.get('/ultimoLance', (req, res) => {
+//       res.json(data)
+//     });
+// });
